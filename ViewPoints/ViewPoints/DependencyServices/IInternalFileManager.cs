8716c22dc1be19b2c8c0334d4674a374969153f2
Martin Pomezný <martin.pomezny@eman.cz>
2018-04-09 15:04:36 +0200

    InternalFileManager added

---
+++ ViewPoints/ViewPoints/DependencyServices/IInternalFileManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewPoints.DependencyServices
+{
+    public interface IInternalFileManager
+    {
+        /// <summary>
+        /// Zjisti zda soubor existuje
+        /// </summary>
+        /// <returns></returns>
+        bool FileExists(string fileName);
+
+        /// <summary>
+        /// Directory zda soubor existuje
+        /// </summary>
+        /// <returns></returns>
+        bool DirectoryExists(string directoryName);
+
+        /// <summary>
+        /// Nacte cely soubor jako pole byte
+        /// </summary>
+        /// <returns>soubor načtený v paměti</returns>
+        Task<byte[]> GetBytes(string fileName);
+
+        /// <summary>
+        /// Uloží celé pole do souboru
+        /// </summary>
+        /// <returns></returns>
+        Task WriteBytes(string fileName, byte[] data);
+
+
+        /// <summary>
+        /// Smaze soubor
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        Task Delete(string fileName);
+    }
+}
